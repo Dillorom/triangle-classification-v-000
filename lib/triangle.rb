3556f560a1131e27b0ed1b536fbc614c8f477144
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-17 22:46:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     @s3 = s3
   end
   def kind(triangle)
-    if (s1 * s2 * s3) = 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 ||(s1 + s3) <= s2
+    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 ||(s1 + s3) <= s2
       begin
         raise TriangleError
          rescue TriangleError => error
